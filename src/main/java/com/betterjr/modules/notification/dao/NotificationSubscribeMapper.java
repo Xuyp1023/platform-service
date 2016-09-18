d29a0c4e5079f4fe6bd3119e746435d1dcc04dc0
root <git4bytter@sina.cn>
2016-09-18 09:26:39 +0800

    Merge branch 'liuwl.platform-service/task-104'

--- src/main/java/com/betterjr/modules/notification/dao/NotificationSubscribeMapper.java
+++ src/main/java/com/betterjr/modules/notification/dao/NotificationSubscribeMapper.java
@@ -19,16 +19,16 @@ public interface NotificationSubscribeMapper extends Mapper<NotificationSubscrib
     @Select("SELECT ID as id, C_PROFILE_NAME as profileName, L_CUSTNO as custNo, C_CUSTNAME as custName " +
             "FROM t_sys_notifi_profile snp " +
             "WHERE (snp.L_CUSTNO IN (" +
-            "SELECT cr1.L_RELATE_CUSTNO FROM t_cust_relation cr1 WHERE cr1.L_CUSTNO = #{custNo} " +
-            "UNION " +
+            "SELECT cr1.L_RELATE_CUSTNO FROM t_cust_relation cr1 WHERE cr1.L_CUSTNO = #{custNo}) " +
+            "OR snp.L_CUSTNO IN (" +
             "SELECT cr2.L_CUSTNO FROM t_cust_relation cr2 WHERE cr2.L_RELATE_CUSTNO = #{custNo} " +
             ") OR snp.L_CUSTNO = 102202018) AND C_SUBSCRIBE_ENABLE = '1'")
     @ResultType(ProfileSubscribeModel.class)
     public Page<ProfileSubscribeModel> selectProfileSubscribe(@Param("custNo") Long custNo);
 
     @Select("SELECT ncp.ID as id, ncp.L_PROFILE_ID as profileId, ncp.C_CHANNEL as channel, (CASE WHEN ns.ID IS NULL THEN 0 ELSE 1 END) as subscribed " +
-            "FROM t_sys_notifi_chan_profile ncp LEFT JOIN t_sys_notifi_sub ns ON ncp.ID = ns.L_CHANNEL_PROFILE_ID " +
-            "WHERE ncp.L_PROFILE_ID = #{profileId} AND ncp.C_BUSIN_STATUS = '1' AND ns.L_OPERID = #{operId} AND ns.L_CUSTNO = #{custNo} AND ns.L_SOURCE_CUSTNO = #{sourceCustNo}")
+            "FROM t_sys_notifi_chan_profile ncp LEFT JOIN t_sys_notifi_sub ns ON (ncp.ID = ns.L_CHANNEL_PROFILE_ID AND ns.L_OPERID = #{operId} AND ns.L_CUSTNO = #{custNo} AND ns.L_SOURCE_CUSTNO = #{sourceCustNo}) " +
+            "WHERE ncp.L_PROFILE_ID = #{profileId} AND ncp.C_BUSIN_STATUS = '1'")
     @ResultType(ChannelSubscribeModel.class)
     public List<ChannelSubscribeModel> selectChannelSubscribe(@Param("operId") Long operId,@Param("custNo") Long custNo,@Param("sourceCustNo") Long sourceCustNo,@Param("profileId") Long profileId);
 }
