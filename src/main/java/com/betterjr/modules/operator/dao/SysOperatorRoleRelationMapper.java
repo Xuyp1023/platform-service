6bb80cdc414acf27e6ca8f5c4a428048d0441676
Hubl <hubl@bytter.com>
2016-10-18 10:36:02 +0800

    task-108

--- src/main/java/com/betterjr/modules/operator/dao/SysOperatorRoleRelationMapper.java
+++
@@ -1,9 +0,0 @@
-package com.betterjr.modules.operator.dao;
-
-import com.betterjr.common.annotation.BetterjrMapper;
-import com.betterjr.mapper.common.Mapper;
-import com.betterjr.modules.operator.entity.SysOperatorRoleRelation;
-
-@BetterjrMapper
-public interface SysOperatorRoleRelationMapper extends Mapper<SysOperatorRoleRelation> {
-}
